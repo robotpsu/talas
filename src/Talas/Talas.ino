38d2981fd1a754f1a0cd3eda239069aacfefeda2
Vasiliy Polyakov <vp@psu.ru>
2017-09-01 16:22:57 +0500

    change pin numbers

--- src/Talas/Talas.ino
+++ src/Talas/Talas.ino
@@ -10,29 +10,29 @@
 //#define PIN_KEY     22
 //#define PIN_STATE   24
 // Driver
-#define PIN_IN1     32
-#define PIN_IN2     34
-#define PIN_IN3     36
-#define PIN_IN4     38
+#define PIN_IN1     23
+#define PIN_IN2     27
+#define PIN_IN3     31
+#define PIN_IN4     35
 #define PIN_ENA     11 // PWM
 #define PIN_ENB     12 // PWM
 // Turret
-#define PIN_LASER   40
-#define PIN_SERVO_H 42
-#define PIN_SERVO_V 44
+#define PIN_LASER   49
+#define PIN_SERVO_H 25
+#define PIN_SERVO_V 29
 // Health indicator
-#define PIN_HEALTH0 48
-#define PIN_HEALTH1 46
-#define PIN_HEALTH2 50
-#define PIN_HEALTH3 52
+#define PIN_HEALTH0 33
+#define PIN_HEALTH1 37
+#define PIN_HEALTH2 41
+#define PIN_HEALTH3 45
 // Hit detector
-#define PIN_HIT    A15 // analog
+#define PIN_HIT    A11 // analog
 
 Talas talas(TALAS_NAME);
 
 void setup() {
   talas.attachHealth(PIN_HEALTH0, PIN_HEALTH1, PIN_HEALTH2, PIN_HEALTH3);
-  talas.attachDriver(PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4, PIN_ENA, PIN_ENB);
+  talas.attachDriver(PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4);
   talas.attachTurret(PIN_LASER, PIN_SERVO_H, PIN_SERVO_V);
   talas.attachHitDetector(PIN_HIT);
   talas.attachBluetooth();
